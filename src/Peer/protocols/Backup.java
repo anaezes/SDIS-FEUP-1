9ae09616feb760972b59efe48e45b18c45441aff
Fr0sk <the.fr0sk@gmail.com>
2018-04-02 16:16:57 +0100

    Making backup wait until all threads end to respond to client

--- src/Peer/protocols/Backup.java
+++ src/Peer/protocols/Backup.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import static java.lang.Thread.sleep;
@@ -42,15 +45,12 @@ public class Backup {
             String fileId = Utils.getEncodeHash(file.getName()+Long.toString(file.lastModified()));
 
             FileData fileData = new FileData(fileId, replicationDegree, chunkQueue);
-            new Thread(() -> {
-                try {
-                    handle(fileData);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            try {
+                handle(fileData);
+            } catch (InterruptedException e) {
+                Logger.getGlobal().warning("Exception when sending chunks: " + e.getLocalizedMessage());
+            }
+
 
             Logger.getGlobal().info("Finished backup...");
         } catch (IOException e) {
@@ -59,7 +59,8 @@ public class Backup {
     }
 
 
-    private void handle(FileData fileData) throws InterruptedException, IOException {
+    private void handle(FileData fileData) throws InterruptedException {
+        ExecutorService es = Executors.newCachedThreadPool();
         while (fileData.getChunkData().size() > 0) {
             if (chunksPending >= WINDOWSIZE) {
                     sleep(50);
@@ -70,8 +71,10 @@ public class Backup {
             PutChunkMessage message =  new PutChunkMessage(Peer.PROTOCOL_VERSION, peer.getPeerId(), fileData.getFileId(),
                     chunkData.getChunkNo(), fileData.getRepDeg(), chunkData.getChunkData());
             chunksPending++;
-            new Thread(() -> sendChunk(message, 0, RESEND_TIMEOUT)).start();
+            es.execute(new Thread(() -> sendChunk(message, 0, RESEND_TIMEOUT)));
         }
+        // Waits for all threads to finish or until the maximum number of seconds all retries would take + 1 second
+        es.awaitTermination(RESEND_TIMEOUT * (long)Math.pow(2, NUM_RETRIES) + 1000, TimeUnit.MILLISECONDS);
     }
 
     private void sendChunk(PutChunkMessage message, int count, int timeout) {
